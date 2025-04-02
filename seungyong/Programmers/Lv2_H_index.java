53aed9c2d65f01800f98c1c0c67795eb59695fb1
sso9594 <sso9594@naver.com>
2025-04-02 17:45:25 +0900

    [Programmers-Lv2]: H Index

---
+++ seungyong/Programmers/Lv2_H_index.java
@@ -0,0 +1,21 @@
+package seungyong.Programmers;
+
+import java.util.*;
+
+class Lv2_H_index {
+    public int solution(int[] citations) {
+        int answer = 0;
+        // n번이상 인용된 논문이 n개이상이면 n은 H-Index 후보군
+        Arrays.sort(citations);
+        for(int i=0; i<citations.length; i++){
+            int count = citations.length - i;
+            if(citations[i] >= count){
+                if(answer < count){
+                    answer = count;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
