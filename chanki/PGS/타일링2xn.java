f112b294db5e1b2b17e85a825e59f3b0b03e3e53
chanki00 <rlacksrl00@gmail.com>
2025-04-16 09:07:03 +0900

    [Programmers-Lv2]: 2 x n 타일링

---
+++ chanki/PGS/타일링2xn.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+public class 타일링2xn {
+
+	class Solution {
+
+		int MOD = 1_000_000_007;
+
+		public int solution(int n) {
+			int answer = 0;
+
+			int[] dp = new int[n + 1];
+
+			dp[0] = 1;
+			dp[1] = 1;
+
+			for (int i = 2; i <= n; ++i) {
+				dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+			}
+
+			return dp[n];
+		}
+	}
+}
